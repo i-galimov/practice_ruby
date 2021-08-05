a2af4336cef27edfe2edf2a4d7a8b835bdb0d5f2
Ilshat Galimov <86232244+ilshat1kzn@users.noreply.github.com>
2021-08-05 10:04:55 +0300

    Create class_Animal.rb

---
+++ class_Animal.rb
@@ -0,0 +1,9 @@
+# Создайте класс Animal (метод инициализации должен выводить "Grrr!" (без кавычек), а затем создайте объект dog класса Animal.
+
+class Animal
+ def initialize 
+  puts "Grrr!"
+ end
+end
+
+dog = Animal.new
